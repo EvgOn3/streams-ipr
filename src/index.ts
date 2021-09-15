77eaebe911c609b4c3af9b38c9a153adb98ff949
Evgeniy Tretyakov <e.tretyakov@gardatech.ru>
2021-09-16 01:18:26 +0300

    pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as http from 'http';
+import { pipeline } from 'stream'
 import { createSpellCheckTransformStream, getStatic } from './services';
 const port = 3030
 
@@ -8,11 +9,20 @@ const server = http
       getStatic(res);
     }
     if (req.method === 'POST' && req.url === '/api/file/send') {
+      
       const encoding = 'utf8';
-    //  res.setHeader('Content-Disposition', 'form-data; name="myFile"; filename="foo.txt"');
+      res.setHeader('Content-Disposition', 'form-data; name="myFile"; filename="foo.txt"');
       res.setHeader('Content-Type', 'text/plain; charset=utf8');
        
-      req.pipe(createSpellCheckTransformStream(encoding)).pipe(res);
+      pipeline(req, createSpellCheckTransformStream(encoding), res, (err)=>{
+        if (err) {
+          console.error('Pipeline failed', err);
+          res.statusCode = 500;
+          res.end('Пятисотая')
+        } else {
+          console.log('Pipeline succeeded');
+        }
+      });
     }
   })
   .listen(port, () => console.log(`Сервер запущен. Порт ${port}`));
--- src/services.ts
+++ src/services.ts
@@ -3,18 +3,19 @@ import * as fs from 'fs';
 import { Transform } from 'stream';
 import spellCheck from './spellChek';
 
-export const getStatic = (res: http.OutgoingMessage) => {
+export const getStatic = (res: http.ServerResponse) => {
   try {
     fs.createReadStream(__dirname + '/index.html').pipe(res);
   } catch (e) {
     console.log(e);
+    res.statusCode = 500;
+    res.end('Пятисотая')
   }
 };
 
 export const createSpellCheckTransformStream = (
   fileEncoding: 'utf8'
 ): Transform => {
-  try {
     const spellCheckStream = new Transform({
       transform(chunk, encoding, callback) {
         this.push(spellCheck(chunk.toString(), fileEncoding));
@@ -23,7 +24,4 @@ export const createSpellCheckTransformStream = (
     });
 
     return spellCheckStream;
-  } catch (e) {
-    console.log(e);
-  }
 };
